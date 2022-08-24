8dd81b14cbe196e9e19aaa89b9304ed5d42f897f
diana-rhodes <105621781+diana-rhodes@users.noreply.github.com>
2022-08-24 11:51:58 -0500

    Replaced "Guest" with "Away"

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 let homeScore = 0
-let guestScore = 0
-
+let awayScore = 0
 
 function highlightLeader() { // call every time the score is updated
-    if (!(homeScore == 0 && guestScore == 0)) {
-        if (homeScore > guestScore) { // if the Home team is in the lead
+    if (!(homeScore == 0 && awayScore == 0)) {
+        if (homeScore > awayScore) { // if the Home team is in the lead
             // highlight guest score and remove any highlight from home score
             document.getElementById("homeScoreEl").style.color = "#50D8D7";
-            document.getElementById("guestScoreEl").style.color = "#BFCDE0";
-        } else if (homeScore == guestScore) { // if the game is tied
-            // highlight both guest score and home score; neither team is in the lead per se, but I think it's still exciting to emphasize the fact that it's anyone's game
+            document.getElementById("awayScoreEl").style.color = "#BFCDE0";
+        } else if (homeScore == awayScore) { // if the game is tied
+            // highlight both guest score and home score
+            // neither team is in the lead per se, 
+            // but I think it's still exciting to emphasize the fact that it's anyone's game
             document.getElementById("homeScoreEl").style.color = "#50D8D7";
-            document.getElementById("guestScoreEl").style.color = "#50D8D7";
+            document.getElementById("awayScoreEl").style.color = "#50D8D7";
         } else { // if the guest team is in the lead
             // highlight guest score and remove any highlight from home score
             document.getElementById("homeScoreEl").style.color = "#BFCDE0";
-            document.getElementById("guestScoreEl").style.color = "#50D8D7";
+            document.getElementById("awayScoreEl").style.color = "#50D8D7";
         } 
     }
 }
@@ -40,30 +41,30 @@ function incrementHomeBy3() {
 }
 
 // Increment Guest team's score and update the text element to match
-function incrementGuestBy1() {
-    guestScore ++
-    document.getElementById("guestScoreEl").textContent = guestScore
+function incrementAwayBy1() {
+    awayScore ++
+    document.getElementById("awayScoreEl").textContent = awayScore
     highlightLeader()
 }
 
-function incrementGuestBy2() {
-    guestScore += 2
-    document.getElementById("guestScoreEl").textContent = guestScore
+function incrementAwayBy2() {
+    awayScore += 2
+    document.getElementById("awayScoreEl").textContent = awayScore
     highlightLeader()
 }
 
-function incrementGuestBy3() {
-    guestScore += 3
-    document.getElementById("guestScoreEl").textContent = guestScore
+function incrementAwayBy3() {
+    awayScore += 3
+    document.getElementById("awayScoreEl").textContent = awayScore
     highlightLeader()
 }
 
-// Reset both team's scores to 0, and remove any highlighting (while 0 and 0 is a tie, I don't think that's exciting enough to warrant highlighting it)
+// Reset both team's scores to 0, and remove any highlighting
 function newGame() {
-    guestScore = 0
-    document.getElementById("guestScoreEl").textContent = guestScore
+    awayScore = 0
+    document.getElementById("awayScoreEl").textContent = awayScore
     homeScore = 0 
     document.getElementById("homeScoreEl").textContent = homeScore
     document.getElementById("homeScoreEl").style.color = "#BFCDE0";
-    document.getElementById("guestScoreEl").style.color = "#BFCDE0";
+    document.getElementById("awayScoreEl").style.color = "#BFCDE0";
 }
